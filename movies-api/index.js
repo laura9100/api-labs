8a7dcc405196154916a55da2b3ef87d9cdf3c6f5
Laura McBride <20084164@mail.wit.ie>
2021-11-15 12:34:06 +0000

    Get Genres added

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
+app.use('/api/genres', genresRouter);
 app.use('/api/movies', moviesRouter);
 
 app.listen(port, () => {
