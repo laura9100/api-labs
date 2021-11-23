b2e55bcd513f6706ee57eacdcee4d1cf47b862ca
Laura McBride <20084164@mail.wit.ie>
2021-11-23 12:45:45 +0000

    Mongo lab User Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,6 +4,7 @@ import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
 import './db';
 import './seedData'
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 
 app.use('/api/genres', genresRouter);
 app.use('/api/movies', moviesRouter);
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
