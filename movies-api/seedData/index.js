77df9faab110c46dc2545b8ddaeca9dc73bbde89
Laura McBride <20084164@mail.wit.ie>
2021-11-30 12:53:24 +0000

    Topic 10 - Update Movies Data/Route

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -3,6 +3,8 @@ import users from './users';
 import dotenv from 'dotenv';
 import genreModel from '../api/genres/genreModel';
 import genres from './genres';
+import movieModel from '../api/movies/movieModel';
+import movies from './movies.js';
 
 dotenv.config();
 
@@ -27,10 +29,21 @@ async function loadGenres() {
     console.error(`failed to Load genre Data: ${err}`);
   }
 }
+export async function loadMovies() {
+  console.log('load seed data');
+  console.log(movies.length);
+  try {
+    await movieModel.deleteMany();
+    await movieModel.collection.insertMany(movies);
+    console.info(`${movies.length} Movies were successfully stored.`);
+  } catch (err) {
+    console.error(`failed to Load movie Data: ${err}`);
+  }
+}
 
 
 if (process.env.SEED_DB) {
   loadUsers();
   loadGenres();
-  
+  loadMovies()
 }
